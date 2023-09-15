c8cdbf06f6117b0f8cbe15f18a34a17654582ab9
ibishara <87097444+ibishara@users.noreply.github.com>
2023-09-14 22:40:49 -0700

    add option to add repo:package as arguments

--- setup_R_environment.sh
+++ setup_R_environment.sh
@@ -2,32 +2,49 @@
 
 setup_R_environment() {
   env_name="$1"
-  pkg_file="$2"
+  pkg_input="$2"
 
+  # Check if conda environment already exists
   env_exists=$(conda env list | awk '{print $1}' | grep -w $env_name)
   if [[ -z $env_exists ]]; then
+    # If the environment doesn't exist, create it
     conda create -n $env_name r-base -y 2>&1 | tee install.log
   fi
 
+  # Activate the environment
   conda activate $env_name
 
+  # Initialize an empty string for R named list
   pkgs_r="list("
-  
-  while IFS="=" read -r key value; do
-    pkgs_r+="${key}=\"${value}\","
-  done < "$pkg_file"
 
-  pkgs_r=${pkgs_r%,}
+  # Check if the package input is a file or string
+  if [[ -f $pkg_input ]]; then
+    # Populate named list from file (format: package=source)
+    while IFS="=" read -r key value; do
+      pkgs_r+="${key}=\"${value}\","
+    done < "$pkg_input"
+  else
+    # Populate named list from string (format: source:package,source:package,...)
+    IFS="," read -ra pairs <<< "$pkg_input"
+    for pair in "${pairs[@]}"; do
+      IFS=":" read -r source pkg <<< "$pair"
+      pkgs_r+="${pkg}=\"${source}\","
+    done
+  fi
+
+  pkgs_r=${pkgs_r%,} # Remove trailing comma
   pkgs_r+=")"
 
+  # R script
   Rscript - <<EOF | tee -a install.log
+# Install devtools for GitHub packages
 if (!require("devtools", quietly = TRUE))
     install.packages("devtools")
 
-options(repos = c(CRAN = "https://cloud.r-project.org"))
-
+# Importing the dictionary of packages
 pkgs <- $pkgs_r
 
+# Install CRAN, Bioconductor, and GitHub packages
 for (pkg in names(pkgs)) {
   source <- pkgs[[pkg]]
   if (!require(pkg, character.only = TRUE)) {
@@ -51,18 +68,22 @@ for (pkg in names(pkgs)) {
 }
 EOF
 
-  while IFS="=" read -r key value; do
-    if [[ "$value" == "conda" ]]; then
-      for channel in "conda-forge" "r" "bioconda"; do
-        echo "Trying to install r-$key through Conda channel $channel..." | tee -a install.log
-        conda run -n $env_name conda install -c $channel r-$key -y 2>&1 | tee -a install.log && break || echo "Could not install r-$key through Conda channel $channel, trying without 'r-' prefix..." | tee -a install.log
-        if [ $? -ne 0 ]; then
-          echo "Trying to install $key through Conda channel $channel..." | tee -a install.log
-          conda run -n $env_name conda install -c $channel $key -y 2>&1 | tee -a install.log && break || echo "Could not install $key through Conda channel $channel" | tee -a install.log
-        fi
-      done
+# Install packages via Conda next
+for pkg in "${!packages[@]}"
+do
+    if [ "${packages[$pkg]}" == "conda" ]; then
+        for channel in "conda-forge" "r" "bioconda"
+        do
+            echo "Trying to install r-$pkg through Conda channel $channel..." | tee -a install.log
+            conda run -n $env_name conda install -c $channel r-$pkg -y 2>&1 | tee -a install.log && break || echo "Could not install r-$pkg through Conda channel $channel, trying without 'r-' prefix..." | tee -a install.log
+            if [ $? -ne 0 ]; then
+                echo "Trying to install $pkg through Conda channel $channel..." | tee -a install.log
+                conda run -n $env_name conda install -c $channel $pkg -y 2>&1 | tee -a install.log && break || echo "Could not install $pkg through Conda channel $channel" | tee -a install.log
+            fi
+        done
     fi
-  done < "$pkg_file"
+done
+
 }
 
 setup_R_environment $1 $2
